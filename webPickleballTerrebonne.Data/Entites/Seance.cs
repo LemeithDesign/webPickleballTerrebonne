1da388782988e5358734fd6fa993a3ce827e4288
Felix Seguin <77398358+felixseguin@users.noreply.github.com>
2025-10-12 08:04:52 -0400

    Changement nom projet

---
+++ webPickleballTerrebonne.Data/Entites/Seance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using web_PickleballTerrebonne.Data.Entites;
+
+namespace webPickleballTerrebonne.Data.Entites
+{
+    public class Seance
+    {
+        public int IdSeance { get; set; }
+        DayOfWeek Jour { get; set; }
+        TimeOnly HeureDebut { get; set; }
+        TimeOnly heureFin { get; set; }
+
+        public int QtePlaceMaximale { get; set; }
+        public int QtePlaceChoisi { get; set; }
+
+        public int TerrainId { get; set; }
+        public Terrain Terrain { get; set; } = default!;
+
+        public int ResponsableId { get; set; }
+        public Membre Responsable { get; set; } = default!;
+
+        //public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+    }
+}
---
+++ webPickleballTerrebonne.Data/Entites/Terrain.cs
@@ -0,0 +1,16 @@
+﻿namespace webPickleballTerrebonne.Data.Entites
+{
+    public class Terrain
+    {
+        public int IdTerrain { get; set; }
+        public string Nom { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public string Adresse { get; set; } = string.Empty;
+        public string Ville { get; set; } = string.Empty;
+        public string CodePostal { get; set; } = string.Empty;
+
+
+        //public ICollection<Seance> Seances { get; set; } = new List<Seance>();
+    }
+}
