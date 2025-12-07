c85a15e64aed0de36adbc1e66a1ad2ffc014ef9a
Felix Seguin <felix.a.seguin@gmail.com>
2025-12-07 09:33:16 -0500

    Css Changes

---
+++ webPickleballTerrebonne/wwwroot/js/Acceuil/Animation.js
@@ -0,0 +1,10 @@
+﻿//document.addEventListener("DOMContentLoaded", (event) => {
+//    gsap.to(".pbtImageTxt", {
+//        y: 300,                // descend de 300px
+//        duration: 0.8,         // durée de la descente
+//        ease: "bounce.out",    // effet rebond
+//        repeat: -1,            // répète indéfiniment
+//        yoyo: true             // remonte après chaque descente
+//    });
+
+//});
