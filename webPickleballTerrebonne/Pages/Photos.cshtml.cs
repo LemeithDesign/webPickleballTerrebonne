a436f63afbce07f1d1730c469b53dda9f1d5d0f3
Felix Seguin <77398358+felixseguin@users.noreply.github.com>
2025-12-13 10:03:47 -0500

    Acceuil et Photo - Terminé

---
+++ webPickleballTerrebonne/Pages/Photos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webPickleballTerrebonne.Pages
+{
+    public class PhotosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- webPickleballTerrebonne/wwwroot/js/Acceuil/Animation.js
+++ webPickleballTerrebonne/wwwroot/js/Acceuil/Animation.js
@@ -1,10 +1,37 @@
-﻿//document.addEventListener("DOMContentLoaded", (event) => {
-//    gsap.to(".pbtImageTxt", {
-//        y: 300,                // descend de 300px
-//        duration: 0.8,         // durée de la descente
-//        ease: "bounce.out",    // effet rebond
-//        repeat: -1,            // répète indéfiniment
-//        yoyo: true             // remonte après chaque descente
-//    });
+﻿document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.from(".pbtImageTxt",
+        {
+            x: -300,
+            y: 200,
+            duration: 1,
+            opacity: 0
+        });
 
-//});
+
+    // Balle bondissante:
+    //let fenetre = document.querySelector('.pbtImage');
+    //let rect = fenetre.getBoundingClientRect();
+    //let largFenetre = rect.width;
+    //let hauteurFenetre = rect.height;
+
+    //let balle = document.querySelector('.pbtBalle img');
+    //let rectBalle = balle.getBoundingClientRect();
+    //let hauteurBalle = rectBalle.height;
+    //let largeurBalle = rectBalle.width
+
+    //let startX = largFenetre * 0.25;
+    //let endX = largFenetre - largeurBalle;
+
+    //let deltaY = hauteurFenetre - hauteurBalle;
+
+    //console.log(startX);
+
+    //gsap.set(".pbtBalle", { x: startX, y: 0, opacity: 0 })
+
+    //const tl = gsap.timeline({ repeat: -1 });
+
+
+    //tl.to(".pbtBalle", { opacity: 1, duration: 1 })
+    //tl.to(".pbtBalle", { y: deltaY, duration: 1 })
+    //tl.to(".pbtBalle", { x: endX, duration: 1 })
+});
